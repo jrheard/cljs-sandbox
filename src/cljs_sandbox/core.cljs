ed070b2a67f2d9d4b97fc2760ea2042cc2553e7d
jrheard <jrheard@cs.stanford.edu>
2016-02-13 11:37:48 -0800

    some functions

--- src/cljs_sandbox/core.cljs
+++ src/cljs_sandbox/core.cljs
@@ -5,9 +5,51 @@
                    [schema.core :as sm])
   (:use [cljs.core.async :only [chan <! >! put!]]))
 
+(sm/defschema Square {:x           s/Int
+                      :y           s/Int
+                      :side-length s/Int
+                      ; oh god i don't know how to do math what am i signing up for
+                      :angle       s/Num
+                      :speed       s/Num})
+
+(sm/defn generate-square []
+  ; TODO use document width
+  {:x           (rand-int 1000)
+   :y           (rand-int 1000)
+   :side-length (max 10 (rand-int 100))
+   :angle       (rand-int 360)
+   :speed       (rand-int 10)})
+
+(sm/defn next-square-state :- Square
+  [sq :- Square]
+  (-> sq
+      (update-in :x (* (:speed sq) (Math/cos (:angle sq))))
+      (update-in :y (* (:speed sq) (Math/sin (:angle sq)))))
+  {:x (+ (:x sq))}
+  )
+
+(defn draw-square [square]
+  [:rect {:x (square :x)
+          :y (square :y)
+          :width (square :side-length)
+          :height (square :side-length)}]
+  )
+
+(defn draw-state [state]
+  [:svg {:width 1000 :height 1000}
+   (let [state @state]
+     (for [[index square] (map-indexed vector (:squares state))]
+       ^{:Key (str "square-" index)} [draw-square square]))])
+
+(def state {:squares (repeatedly 10 generate-square)})
+
 (defn ^:export main []
-  (r/render-component [:div.foo "hello"]
-                      (js/document.getElementById "content")))
+  (r/render-component [draw-state state]
+                      (js/document.getElementById "content"))
+
+
+  ; TODO - kill all goblocks, start up new ones
+  )
 
 
 (comment
