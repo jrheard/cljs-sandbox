26c7cb57eac31a82b57cfae6f1a15e2f70c24745
jrheard <jrheard@cs.stanford.edu>
2016-02-21 10:32:50 -0800

    notes

--- src/cljs_sandbox/reagent_sandbox.cljs
+++ src/cljs_sandbox/reagent_sandbox.cljs
@@ -27,11 +27,45 @@
      (for [[index item] (map-indexed vector state)]
        ^{:key (str "thing-" index)} [draw-rect index item])]))
 
+
+;;;; from https://github.com/Day8/re-frame/wiki/When-do-components-update%3F
+
+(defn greet-number
+  "I say hello to an integer"
+  [num]                             ;; an integer
+  (js/console.log "greet-number")
+  [:div (str "Hello #" num)])       ;; [:div "Hello #1"]
+
+
+(defn more-button
+  "I'm a button labelled 'More' which increments counter when clicked"
+  [counter]                             ;; a ratom
+  (js/console.log "more-button")
+  [:div  {:class "button-class"
+          :on-click  #(swap! counter inc)}   ;; increment the int value in counter
+   "More"])
+
+
+(defn parent
+  []
+  (js/console.log "parent init")
+  (let [counter  (reagent.ratom/atom 1)]    ;; the render closes over this state
+    (fn  parent-renderer
+      []
+      (js/console.log "parent run")
+      [:div
+       [more-button counter]            ;; no @ on counter
+       [greet-number @counter]])))      ;; notice the @. The prop is an int
+
 (defn ^:export main []
-  (r/render-component [draw-rects state]
+  (r/render-component [parent]
+
                       (js/document.getElementById "content")))
 
 ; .... well, so far when you update this atom, only the relevant rectangle gets redrawn, not all of 'em.
 (comment
   (swap! state assoc 1 4)
   )
+
+
+
